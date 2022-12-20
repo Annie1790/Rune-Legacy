049f13c2b73c1e1bc04e9d0237ec69e3bdddf3d8
Annie1790 <budayaniko@outlook.com>
2022-12-20 00:40:07 +0000

    js skeleton wip

--- game_main_menu_script.js
+++ game_main_menu_script.js
@@ -6,18 +6,40 @@ class Character {
         this.level = +characterLevel.innerHTML;
     }
 
+    heal(amount) {
+        this.health += amount;
+        this.show();
+    }
+
+    damage(amount) {
+        this.health -= amount;
+        this.show();
+    }
+
+    show() {
+        characterName.innerHTML = this.name;
+        characterHealth.innerHTML = this.health;
+        characterExperience.innerHTML = this.experience;
+        characterLevel.innerHTML = this.level;
+    }
 
 }
 
-class Scenario {
-    constructor(scenario) {
-        this.scenario = scenario;
+class Event {
+    constructor(name, description, option1, option2) {
+        this.name = name;
+        this.description = description;
+        this.option1 = option1;
+        this.option2 = option2;
     }
 
-    switch (scenario) {
-        case first: 
-        
+    show() {
+        scenarioName.innerHTML = this.name;
+        scenarioDescription.innerHTML = this.description;
+        option1.innerHTML = this.option1;
+        option2.innerHTML = this.option2;
     }
+
 }
 
 
@@ -32,7 +54,22 @@ const characterLevel = document.querySelector("[data-level]");
 
 let player1 = new Character()
 
+
+
+let event1 = new Event("Alex Returns", "The big t rex farts a big. what would you do?", "run", "stay")
+let event2 = new Event("Aniko farts", "Aniko is so digusting she farts a lot. Do you love her?", "yes","no")
+let eventArray = [event1,event2];
+function selectEvent() {
+    let index = Math.floor(Math.random()*eventArray.length);
+    eventArray[index].show()
+}
+
+
+
+
+
 window.addEventListener("load", function() {
     const searchParams = new URLSearchParams(window.location.search);
     characterName.innerText = searchParams.get("name")
+    selectEvent();
 })
