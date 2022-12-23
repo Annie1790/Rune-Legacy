440df4652592bc7cf36e28d589d1a712e4281feb
Annie1790 <budayaniko@outlook.com>
2022-12-23 17:24:34 +0000

    character decision function is done and works

--- player_name_script.js
+++ player_name_script.js
@@ -2,7 +2,7 @@
 function onOkButtonClicked() {
     const nameField = document.querySelector("[data-ask-name]");
     alert(`Welcome ${nameField.value}! Enjoy the game!`);
-    window.location = `game_main_menu.html?name=${nameField.value}`
+    window.location = `./game_main_menu.html?name=${nameField.value}`
 }
 //Called when the page is loaded
 function setup() {
